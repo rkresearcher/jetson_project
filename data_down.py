602538ab7159cf807ad06377852f2b6c3937a67f
Rahul Kumar <rahul007ks@gmail.com>
2022-05-15 13:34:41 +0530

    First data

--- data_down.py
+++ data_down.py
@@ -4,5 +4,6 @@ import xlrd
 fl_file = xlrd.open_workbook("Data Set Link sources.xlsx")
 sheet = fl_file.sheet_by_index(0)
 print (sheet.cell_value(0,0))
-for i in range(sheet.nrows):
-	print (sheet.cell_value(i,1))
+for i in range(2,sheet.nrows):
+	link = sheet.cell_value(i,1)
+	os.system("youtube-dl "+ link)
