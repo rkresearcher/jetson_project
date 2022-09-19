412f6d00fd7f70006eafeefe0d78ce5abdb752ea
rkresearcher <rahul007ks@gmail.com>
2022-09-19 13:02:45 +0530

    sh code

---
+++ im.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential cmake unzip pkg-config
+sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install libv4l-dev libxvidcore-dev libx264-dev
+sudo apt-get install libgtk-3-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install python3-dev
+
